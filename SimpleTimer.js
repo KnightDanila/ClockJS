298ec97b7efc1490cc3c356a3860bae12c0594e0
KnightDanila <KnightDanila@i.ua>
2019-08-16 23:35:11 +0300

    updates :bird:

--- SimpleTimer.js
+++ SimpleTimer.js
@@ -3,42 +3,38 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+/* 
+    Created on : 18.12.2017, 21:19:29
+    Author     : KnightDanila
+*/
 
 
-var SimpleTimer = {
-    divID: "SimpleTimer",
-    mainBodyID: "", //
+var SimpleTimer = function () {
+    var mainID = "",
+            start = new Date().getTime();
 
-    start: new Date().getTime(), //
-
-    init: function (mainBodyID) {
+    this.init = function (mainID) {
         this.start = new Date().getTime();
-        this.mainBodyID = mainBodyID;
-        var div = document.createElement("div");
-        div.setAttribute("id", this.divID);
-        div.className = 'SimpleTimer';
-        document.getElementById(this.mainBodyID).appendChild(div);
-
-        setInterval(function () {
-            SimpleTimer.showTime(); // this will run after every 1 seconds
-        }, 1000);
-    },
-    startAgain: function () {
+        this.mainID = mainID;
+        //document.getElementById(this.mainID).className = 'simpleTimer';
+        setInterval(
+                this.showTime.bind(this) // this will run after every 1 seconds
+                , 1000);
+    };
+    this.startAgain = function () {
         this.start = new Date().getTime();
-    },
-    getTime: function () {
+    };
+    this.getTime = function () {
         return (new Date().getTime()) - this.start;
-    },
-    showTime: function () { //https://www.youtube.com/watch?v=LUapZhcsdx8
+    };
+    this.showTime = function () { //https://www.youtube.com/watch?v=LUapZhcsdx8
         var date = new Date(this.getTime());
         var values = [date.getUTCHours(), date.getMinutes(), date.getSeconds()];
 
         for (var i = 0; i < 3; i++)
             if (values[i] < 10)
                 values[i] = "0" + values[i];
-        
-        document.getElementById(this.divID).innerHTML = values.join(":");;
-    }
 
-
+        document.getElementById(this.mainID).innerHTML = values.join(":");
+    };
 };
